d7977ee44d3ee067f0f983a440b32500f2d2e752
GrumpyMule1573 <catherine_mowbray@yahoo.co.uk>
2022-11-01 18:12:27 +0000

    reordered calling drink_options.find_drink(drink_choice) to ensure that the "sorry not available" feedback does not show with a call for a report

--- Day_16/main.py
+++ Day_16/main.py
@@ -9,15 +9,16 @@ money_taken = MoneyMachine()
 
 while is_it_on:
     drink_choice = input(f"Which drink would you like? {drink_options.get_items()}: ").lower()
-    # drink_ordered is a MenuItem() object from drink_options.find_drink(drink_choice)
-    drink_ordered = drink_options.find_drink(drink_choice)
     if drink_choice == "report":
         coffee_machine.report()
         money_taken.report()
     elif drink_choice == "off":
         is_it_on = False
-    elif drink_ordered is not None:
-        if coffee_machine.is_resource_sufficient(drink_ordered):
-            if money_taken.make_payment(drink_ordered.cost):
-                coffee_machine.make_coffee(drink_ordered)
+    else:
+        # drink_ordered is a MenuItem() object from drink_options.find_drink(drink_choice)
+        drink_ordered = drink_options.find_drink(drink_choice)
+        if drink_ordered is not None:
+            if coffee_machine.is_resource_sufficient(drink_ordered):
+                if money_taken.make_payment(drink_ordered.cost):
+                    coffee_machine.make_coffee(drink_ordered)
 
