1d0bd9d732aeffdfa2636faaa08dc3311045b760
GrumpyMule1573 <catherine_mowbray@yahoo.co.uk>
2022-11-01 17:46:29 +0000

    updates to error checking print statements and money report added

--- Day_16/main.py
+++ Day_16/main.py
@@ -9,15 +9,14 @@ money_taken = MoneyMachine()
 
 while is_it_on:
     drink_choice = input(f"Which drink would you like? {drink_options.get_items()}: ").lower()
-    # drink_ordered returns a MenuItem() object
+    # drink_ordered is a MenuItem() object from drink_options.find_drink(drink_choice)
     drink_ordered = drink_options.find_drink(drink_choice)
     if drink_choice == "report":
         coffee_machine.report()
+        money_taken.report()
     elif drink_choice == "off":
         is_it_on = False
     elif drink_ordered is not None:
-        print(drink_ordered.ingredients)
         if coffee_machine.is_resource_sufficient(drink_ordered):
             coffee_machine.make_coffee(drink_ordered)
-            print("drink made")
             money_taken.make_payment(drink_ordered.cost)
