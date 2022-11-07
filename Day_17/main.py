9147b7433ce94edfc4157fc21faf46dd0e79e8cd
GrumpyMule1573 <catherine_mowbray@yahoo.co.uk>
2022-11-07 16:25:54 +0000

    Updated quiz to be able to use other question sources, adjusted formatting

--- Day_17/main.py
+++ Day_17/main.py
@@ -1,6 +1,6 @@
 from question_model import Question
 from quiz_brain import QuizBrain
-from data import question_data
+from data import question_data, second_questions
 
 q_bank = []
 for question in question_data:
@@ -8,8 +8,15 @@ for question in question_data:
     q_answer = question["answer"]
     q_bank.append(Question(q_text, q_answer))
 
-quiz = QuizBrain(q_bank)
+q_bank_2 = []
+for question in second_questions:
+    q_text = question["question"]
+    q_answer = question["correct_answer"]
+    q_bank_2.append(Question(q_text, q_answer))
+
+quiz = QuizBrain(q_bank_2)
 quiz.next_question()
+
 
 
 
--- Day_17/quiz_brain.py
+++ Day_17/quiz_brain.py
@@ -5,6 +5,7 @@ class QuizBrain:
     def __init__(self, q_input):
         self.question_number = 0
         self.question_list = q_input
+        self.score = 0
 
     def next_question(self):
         correct = True
@@ -13,13 +14,14 @@ class QuizBrain:
                                  f"\nYour answer (true/false): ").lower()
             if ask_question in ("true", "false"):
                 if ask_question == self.question_list[self.question_number].answer.lower():
-                    if self.question_number == len(self.question_list)-1:
-                        print(f"Congratulations, you have answered all {self.question_number+1} questions correctly!")
-                        break
                     self.question_number += 1
-                    print(f"That's right! Correct answers {self.question_number}/{self.question_number}")
-                else:
-                    print(f"Incorrect. Final score is {self.question_number}/{self.question_number+1}")
+                    self.score += 1
+                    print(f"That's right! Correct answers {self.score}/{self.question_number}\n")
+                elif ask_question != self.question_list[self.question_number].answer.lower():
+                    self.question_number += 1
+                    print(f"Incorrect. Your score is {self.score}/{self.question_number}\n")
+                if self.question_number == len(self.question_list):
+                    print(f"All questions answered! Your final score is {self.score}/{self.question_number}")
                     correct = False
 
 
